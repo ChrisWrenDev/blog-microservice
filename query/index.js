afca36c035c6e3569a8b570938494ad648a70bf5
Chris Wren <dazedpandas@gmail.com>
2023-04-27 20:55:32 +0100

    Added status property to Comments service

--- query/index.js
+++ query/index.js
@@ -6,9 +6,27 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+const posts = {};
+
 app.get("/post", (req, res) => {});
 
-app.post("/events", (req, res) => {});
+app.post("/events", (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === "PostCreated") {
+    const { id, title } = data;
+    post[id] = { id, title, comments: [] };
+  }
+
+  if (type === "CommentCreated") {
+    const { id, content, postId, status } = data;
+
+    const post = posts[postId];
+    post.comments.push({ id, content, status });
+  }
+
+  res.send({});
+});
 
 app.listen(4002, () => {
   console.log("listening on 4002");
