4170822c71ddc14887e6ab61818d3c72428cb6e5
Chris Wren <chris.wren@cef.co.uk>
2023-04-27 08:29:20 +0100

    Handled events in query service

--- query/index.js
+++ query/index.js
@@ -6,9 +6,29 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get("/post", (req, res) => {});
+const posts = {};
 
-app.post("/events", (req, res) => {});
+app.get("/post", (req, res) => {
+  res.send(posts);
+});
+
+app.post("/events", (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === "PostCreated") {
+    const { id, title } = data;
+    post[id] = { id, title, comments: [] };
+  }
+
+  if (type === "CommentCreated") {
+    const { id, content, postId } = data;
+
+    const post = posts[postId];
+    post.comments.push({ id, content });
+  }
+
+  res.send({});
+});
 
 app.listen(4002, () => {
   console.log("listening on 4002");
